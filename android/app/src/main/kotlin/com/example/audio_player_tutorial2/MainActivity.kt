4f5b29feb17b98f99569efc564ce47c45913d902
Code HQ <codeheadq@gmail.com>
2023-05-27 15:35:57 +0430

    first commit

---
+++ android/app/src/main/kotlin/com/example/audio_player_tutorial2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.audio_player_tutorial2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,127 @@
+import 'package:audio_player_tutorial2/utils/utils.dart';
+import 'package:audio_player_tutorial2/widgets/audio_info.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isPlaying = false;
+  late final AudioPlayer player;
+  late final AssetSource path;
+
+  Duration _duration = const Duration();
+  Duration _position = const Duration();
+
+  @override
+  void initState() {
+    initPlayer();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  Future initPlayer() async {
+    player = AudioPlayer();
+    path = AssetSource('audios/ukulele.mp3');
+
+    // set a callback for changing duration
+    player.onDurationChanged.listen((Duration d) {
+      setState(() => _duration = d);
+    });
+
+    // set a callback for position change
+    player.onPositionChanged.listen((Duration p) {
+      setState(() => _position = p);
+    });
+
+    // set a callback for when audio ends
+    player.onPlayerComplete.listen((_) {
+      setState(() => _position = _duration);
+    });
+  }
+
+  void playPause() async {
+    if (isPlaying) {
+      player.pause();
+      isPlaying = false;
+    } else {
+      player.play(path);
+      isPlaying = true;
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const AudioInfo(),
+            const SizedBox(height: 50),
+            Slider(
+              value: _position.inSeconds.toDouble(),
+              onChanged: (value) async {
+                await player.seek(Duration(seconds: value.toInt()));
+                setState(() {});
+              },
+              min: 0,
+              max: _duration.inSeconds.toDouble(),
+              inactiveColor: Colors.grey,
+              activeColor: Colors.red,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(_duration.format()),
+              ],
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: () {
+                    player.seek(Duration(seconds: _position.inSeconds - 10));
+                    setState(() {});
+                  },
+                  child: Image.asset('assets/icons/rewind.png'),
+                ),
+                const SizedBox(width: 20),
+                InkWell(
+                  onTap: playPause,
+                  child: Icon(
+                    isPlaying ? Icons.pause_circle : Icons.play_circle,
+                    color: Colors.red,
+                    size: 100,
+                  ),
+                ),
+                const SizedBox(width: 20),
+                InkWell(
+                  onTap: () {
+                    player.seek(Duration(seconds: _position.inSeconds + 10));
+                    setState(() {});
+                  },
+                  child: Image.asset('assets/icons/forward.png'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
